a6eba2236bf169f758de8b6e84a478a98c2c57de
OtabekMirzakhmedov <otabek.mirzakhmedov@gmail.com>
2025-11-13 12:45:39 +0500

    adding fixes

--- frontend/src/services/course-teachers.service.ts
+++ frontend/src/services/course-teachers.service.ts
@@ -22,6 +22,13 @@ class CourseTeachersService {
     return response.data.data;
   }
 
+  async getMyAssignments(): Promise<CourseTeacher[]> {
+    const response = await api.get<ApiResponse<CourseTeacher[]>>(
+        '/course-teachers/my-assignments'
+    );
+    return response.data.data;
+  }
+
   async getOne(id: number): Promise<CourseTeacher> {
     const response = await api.get<ApiResponse<CourseTeacher>>(
       `/course-teachers/${id}`
